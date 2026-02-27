a5a420633bdc677a154a98ad4bf3535c91850546
shakhawatyt71 <143334763+shakhawatyt71@users.noreply.github.com>
2026-02-27 14:23:59 +0600

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: "kp1",
+    name: "Minimal Oversized T-Shirt",
+    price: 1499,
+    image: "https://via.placeholder.com/400x500",
+    description: "Premium cotton oversized fit."
+  },
+  {
+    id: "kp2",
+    name: "Urban Hoodie",
+    price: 2499,
+    image: "https://via.placeholder.com/400x500",
+    description: "Heavy winter textured hoodie."
+  },
+  {
+    id: "kp3",
+    name: "Classic Panjabi",
+    price: 1999,
+    image: "https://via.placeholder.com/400x500",
+    description: "Bold simplicity crafted."
+  }
+];
