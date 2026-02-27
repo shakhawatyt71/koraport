b17f99f5852bf27e14894e0af51a98fae9e93753
shakhawatyt71 <143334763+shakhawatyt71@users.noreply.github.com>
2026-02-27 14:24:50 +0600

    Update page.js

--- app/products/page.js
+++ app/products/page.js
@@ -1,20 +1,35 @@
-import { products } from "@/data/products";
-import ProductCard from "@/components/ProductCard";
+import { products } from "../../data/products";
+import Link from "next/link";
 
 export default function ProductsPage() {
   return (
-    <main style={{ padding: "60px 40px" }}>
-      <h1 style={{ marginBottom: "40px" }}>Shop All</h1>
+    <div style={{ padding: "60px 40px" }}>
+      <h1>Shop All</h1>
 
       <div style={{
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
-        gap: "30px"
+        gridTemplateColumns: "repeat(auto-fill, minmax(250px,1fr))",
+        gap: "30px",
+        marginTop: "30px"
       }}>
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <Link
+            key={product.id}
+            href={`/products/${product.id}`}
+            style={{
+              textDecoration: "none",
+              background: "white",
+              padding: "20px",
+              borderRadius: "10px",
+              color: "black"
+            }}
+          >
+            <img src={product.image} width="100%" />
+            <h3>{product.name}</h3>
+            <p>{product.price} BDT</p>
+          </Link>
         ))}
       </div>
-    </main>
+    </div>
   );
 }
